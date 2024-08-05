e73295d2f48662279af6ef1466333edc2b7fffe8
itdanjalog <kgs2072@naver.com>
2024-08-05 17:10:12 +0900

    Add files via upload

---
+++ src/day08/step1/Person.java
@@ -0,0 +1,8 @@
+package day08.step1;
+
+// 클래스는 만드는 행위
+// --> 머리속에 있는 추상적인(객체) 개념 를 설계
+public class Person {
+    String name;
+    int age;
+}
---
+++ src/day08/step2/Account.java
@@ -0,0 +1,7 @@
+package day08.step2;
+
+// 계좌[통장] 설계
+public class Account {
+    String number; // 계좌번호
+    int balance; // 예금액
+}
